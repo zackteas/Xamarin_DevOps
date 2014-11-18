3e9f977d8c7592d941274d90335c4b3bb8fe1603
zackteas <zft6616@rit.edu>
2014-11-17 20:48:48 -0500

    Updated data_grabber.py
    Created main function and calcAvgAge function to calculate the average age of a list of actors' ages.

--- data_grabber.py
+++ data_grabber.py
@@ -1,3 +1,33 @@
 """
 Program to filter through and grab data from IMDb and Wikipedia.
 """
+
+def getMovieList():
+  
+  
+def getActorList(str movie):
+  
+  
+def getActorAge(list actors):
+  
+  
+def calcAvgAge(list actorAges):
+  age = 0.0
+  for i in range(len(actors)):
+    age+=actors[i]
+  age=float(age)/len(actors)
+  return age
+  
+def main():
+  movies = getMovieList()
+  print("The movies currently playing in US theaters are: \n=======================================\n")
+  for i in range(len(movies)):
+    actors = []
+    actorsAge = 0
+    actors = getActorList(movies[i])
+    actorAges = getActorAge(actors)
+    avgAge = calcAvgAge(actorAges)
+    print(str(i)+" "+movies[i]+".")
+    print("Average actor age for "+movies[i]+" is "+str(avgAge)".")
+    
+main()
