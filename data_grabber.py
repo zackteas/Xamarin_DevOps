9a263ea4c5c1249126538f829718a3a7dfe13637
zackteas <zft6616@rit.edu>
2014-11-21 00:24:10 -0500

    Updated data_grabber.py
    Finished getActorAge function, getActorList function and continued, but was unable to figure out getMovieList function.

--- data_grabber.py
+++ data_grabber.py
@@ -1,23 +1,63 @@
 """
 Program to filter through and grab data from IMDb and Wikipedia.
 """
-from httplib import *
-from urllib2 import *
+import httplib
+import urllib2
+import json
 
 def getMovieList():
+  movies = []
+  url = "http://www.imdb.com/showtimes/location?ref_=inth_ov_sh_sm"
+  req = urllib2.Request(url)
+  handle = urllib2.urlopen(req)
+  content = handle.read()
+  split_page = content.split("<div class=\"lister-list\">", 1)
+  split_page = split_page[1].split("<div class=\"hidden lister-working\">", 1)
+  
   
   
 def getActorList(str movie):
-  
+  actor_list = []
+  count = 0
+  baseurl = "http://en.wikipedia.org/wiki/"
+  url = baseurl+movie+"_(film)"
+  req = urllib2.Request(url)
+  handle = urllib2.urlopen(req)
+  content = handle.read()
+  actor_section = content.split("<b>Astronaut crew</b>", 1)
+  actor_section = actor_section[1].split("<h2>", 1)
+  actor_section = actor_section[0].split"<li>")
+  for i in range(len(actor_section)):
+    actor_section[i] = actor_section[i].split("<a href=\"/wiki/", 1)
+  for i in range(len(actor_section)):
+    if (len(actor_section[i])>1):
+      actor_section[i] = actor_section[i][1].split("\"",1)
+    if (actor_section[i][0]=='<'):
+      count++
+  for i in range(count):
+    for j in range(len(actor_section)):
+      if (actor_section[i][0]!='<'):
+        actor_list.append(actor_section[i])
+      break
   
 def getActorAge(list actors):
-  
+  actorAges = []
+  for i in len(actors):
+    baseurl = "http://en.wikipedia.org/wiki/"
+    url = baseurl+actors[i]
+    req = urllib2.Request(url)
+    handle = urllib2.urlopen(req)
+    content = handle.read()
+    age_section = content.split("<span class=\"noprint ForceAgeToShow\">(age&nbsp;",1)
+    age_section = age_section[1].split(")</span>",1)
+    actorAges.append(int(age_section))
+  return actorAges
   
 def calcAvgAge(list actorAges):
   age = 0.0
-  for i in range(len(actors)):
+  for i in range(len(actorAges)):
     age+=actors[i]
-  age=float(age)/len(actors)
+  age=float(age)/len(actorAges)
   return age
   
 def main():
@@ -32,4 +72,5 @@ def main():
     print(str(i)+" "+movies[i]+".")
     print("Average actor age for "+movies[i]+" is "+str(avgAge)".")
     
-main()
+if __name__ == '__main__':
+  main()
